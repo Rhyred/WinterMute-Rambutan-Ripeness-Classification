7f0830007f648245502a2c70329d6fe1f3742539
Rhyred <robigold9@gmail.com>
2026-05-27 23:38:19 -0400

    a

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['rhyred.net']
+    allowedHosts: [
+      'rhyred.net' ,
+      'localhost',
+    ]
   }
 })
